b95b35c2c3fdffc22635503b384972dbd254b0da
PavloShutz <pavlo050706@gmail.com>
2022-03-14 13:23:36 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from mathing import calc
+from telegram.ext import Filters, MessageHandler, CommandHandler, Updater
+from pathlib import Path
+
+file = Path.cwd() / 'token.txt'
+data = open(file, 'r')
+info = data.read()
+data.close()
+TOKEN = '5252906753:AAEjVzkESABxH7PmU09dA4xhnXobAtWuvOQ'
+
+updater = Updater(TOKEN)
+
+
+def start(update, context):
+    chat = update.effective_chat
+    context.bot.send_message(chat_id=chat.id,
+                             text='Hello! I\'m math bot. Type /help for more info')
+
+
+def helper(update, context):
+    chat = update.effective_chat
+    context.bot.send_message(chat_id=chat.id, text='Type any operation you want, and that\'s all 😁')
+
+
+def reply_message(update, context):
+    chat = update.effective_chat
+    message = update.message.text
+    try:
+        result = calc(message)
+        text = f"{result}"
+        context.bot.send_message(chat_id=chat.id, text=text)
+    except ZeroDivisionError:
+        context.bot.send_message(chat_id=chat.id, text='Division by zero!')
+    except (IndexError, SyntaxError):
+        context.bot.send_message(chat_id=chat.id, text='Invalid input!!!')
+    except KeyError:
+        context.bot.send_message(chat_id=chat.id, text='Please, type without whitespaces!')
+
+
+disp = updater.dispatcher
+disp.add_handler(CommandHandler('start', start))
+disp.add_handler(CommandHandler('help', helper))
+disp.add_handler(MessageHandler(Filters.all, reply_message))
+
+updater.start_polling()
+updater.idle()
---
+++ mathing.py
@@ -0,0 +1,30 @@
+def calc(expr):
+    operator_function = {
+        '-': lambda x, y: x - y,
+        '+': lambda x, y: x + y,
+        '*': lambda x, y: x * y,
+        '/': lambda x, y: x / y,
+    }
+    nums = list()
+    opers = list()
+
+    tokens = list('(' + expr + ')')
+    while tokens:
+        token = tokens.pop(0)
+
+        if token.isdecimal():
+            nums.append(float(token))
+        else:
+            if token == ')':
+                oper = opers.pop()
+                while opers and oper != '(':
+                    b, a = nums.pop(), nums.pop()
+                    f = operator_function[oper]
+                    nums.append(f(a, b))
+
+                    oper = opers.pop()
+
+            else:
+                opers.append(token)
+
+    return nums[0]
